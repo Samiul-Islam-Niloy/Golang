190c08e31cd8119618d7d9d41061f782de0e4d58
Samiul Islam Niloy <samiul3030@gmail.com>
2022-07-22 20:38:11 +0600

    22

--- 22bitmorejson/main.go
+++ 22bitmorejson/main.go
@@ -15,7 +15,8 @@ type course struct {
 
 func main() {
 	fmt.Println("Welcome to JSON video")
-	EncodeJson()
+	//EncodeJson()
+	DecodeJson()
 }
 
 func EncodeJson() {
@@ -36,6 +37,35 @@ func EncodeJson() {
 	fmt.Printf("%s\n", finalJson)
 }
 
-func DecodeJson()  {
-	
+func DecodeJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"coursename": "ReactJS Bootcamp",
+		"Price": 299,
+		"Platform": "LearnCodeOnline.in",
+		"Tags": ["web-dev", "js"]
+	}
+	`)
+
+	var lcoCourse course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON was valid")
+		json.Unmarshal(jsonDataFromWeb, &lcoCourse)
+		fmt.Printf("%#v\n", lcoCourse)
+	} else {
+		fmt.Println("JSON WAS NOT VALID")
+	}
+
+	// some cases where you just want to add data to key value
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k, v := range myOnlineData {
+		fmt.Printf("Key is %v and value is %v and type is: %T\n", k, v, v)
+	}
 }
